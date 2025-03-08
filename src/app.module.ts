a4a7b415ce14c90fe5e4d4da707b3de51af9c0f5
NgoNgocHuy-IT <huynn@tekup.vn>
2025-03-08 14:29:19 +0700

    fix: import cronjob

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module, RequestMethod, MiddlewareConsumer } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app/controllers/app.controller';
 import { DomainModule } from './app/module/entity.module';
 import { ConfigModule } from './config/config.module';
-import { DbModule } from './db/db.module';
 import { AppLoggerModule } from './logger/logger.module';
 @Module({
-  imports: [ConfigModule, DomainModule, AppLoggerModule],
+  imports: [
+    ConfigModule,
+    DomainModule,
+    AppLoggerModule,
+    ScheduleModule.forRoot(),
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
